00ad10658b5a170f6d0372fa8fc558285c243128
Shahqaan Qasim <08bitsqasim@seecs.edu.pk>
2015-05-22 14:32:42 -0700

    Implement stream but in sync

--- controllers/t4.js
+++ controllers/t4.js
@@ -1,17 +1,32 @@
 var Utils = require('../lib/Utils');
 var async = require('async');
-var RSVP = require('rsvp');
+var Rx = require('rx');
 
 exports.getTitles = function(req, res) {
     if (req.query.address) {
 	if (req.query.address instanceof Array) {
-	    var promises = req.query.address.map(function(url) {
-		return Utils.getTitlePromise(url);
-	    });
-	    RSVP.all(promises).then(function(urls) {
-		res.send({titles: urls});
-	    });
+	    var titles = [];
+	    var requestStream = Rx.Observable.just(req.query.address)
+		.flatMap(function(addr) {
+		    return addr
+		});
 
+	    // TODO: It feels like is working synchronously :-|
+	    var responseStream = requestStream
+		.flatMap(function(requestUrl) {
+		    return Rx.Observable.fromPromise(Utils.getTitleStream(requestUrl));
+		});
+
+	    responseStream.subscribe(function(response) {
+		titles.push(response);
+	    }, function(err) {
+		console.log(err);
+		res.send({err: "error processing your request"}, 500);
+	    }, function(res) {
+		res.send({titles: titles});
+	    });
+	    
+	    
 	}
 	else {
 	    Utils.getTitle(req.query.address, function(title) {
